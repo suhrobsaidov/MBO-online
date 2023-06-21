4b7c7823263e2c570dd4279af5e6b27bf86e9c61
MirsaidovOskar <https://github.com/MirsaidovOskar>
2023-06-21 13:07:40 +0500

    check

--- resources/views/admin/register-edit.blade.php
+++ resources/views/admin/register-edit.blade.php
@@ -28,18 +28,13 @@
                                <div class="form-group">
                                    <label>Give Role</label>
                                    <select name="usertype" class="form-control">
-                                       <option value="user">Юзер</option>    
+                                       <option value="user">Пользователь</option>
                                        <option value="admin">Админ</option>
-                                       <option value="student">Студент</option>
+                                       <option value="moderator">Модератор</option>
                                        <option value="cashier">Кассир</option>
                                    </select>
                                </div>
                                <div><label>Телефон</label><input type="text" name="phone" value="{{ $users->phone }}" class="form-control"></div>
-
-                               <div><label>Дата рож.</label><input type="text" name="birthdate" value="{{ $users->birthdate }}" class="form-control"></div>
-                               <div><label>Уровень</label><input type="text" name="level" value="{{ $users->level }}" class="form-control"></div>
-                               <div><label>Дни</label><input type="text" name="days" value="{{ $users->days }}" class="form-control"></div>
-                               <div><label>Время</label><input type="text" name="time" value="{{ $users->time }}" class="form-control"></div>
                                <button type="submit" class="btn btn-success">Update</button>
                                <a href="/profile" class="btn btn-danger">Cancel</a>
                            </form>
